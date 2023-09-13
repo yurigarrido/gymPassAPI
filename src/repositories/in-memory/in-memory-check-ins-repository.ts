7423c16b02c948732c53b26fe62fceb3330c6eda
Yuri Garrido <y.garrido.yuri@gmail.com>
2023-09-13 11:29:35 -0300

    feat: add get user metrics use case

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -44,4 +44,8 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       .filter((checkIn) => checkIn.user_id === userId)
       .slice((page - 1) * 20, page * 20)
   }
+
+  async countByUserId(userId: string): Promise<number> {
+    return this.items.filter((checkIn) => checkIn.user_id === userId).length
+  }
 }
