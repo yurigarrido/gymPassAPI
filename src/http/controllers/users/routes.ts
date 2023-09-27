5c9becf3fe012b1945ff50274e7c4f3fc60e3147
Yuri Garrido <y.garrido.yuri@gmail.com>
2023-09-26 21:04:03 -0300

    feat: implement refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { verifyJWT } from '../../middlewares/verify-jwt'
 import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   // authenticated
   app.get(
